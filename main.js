149503346f8c43620a783d59a75e6bf31bc78569
NotQuiteMelvin <cebanu5665@gmail.com>
2016-12-14 01:28:51 +0000

    Slight tweak

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
+const scl = 0.05;
+const width = 600;
+const height = 600;
+
 function setup() {
-  const width = 600;
-  const height = 600;
-  createCanvas(width, height);
+  createCanvas(600, 600);
 }
 
-
 function draw() {
-  const scl = 0.05;
-  const width = 600;
-  const height = 600;
   background(0);
   for (let x = 0; x < width; x++) {
     for (let y = 0; y < height; y++) {
@@ -16,7 +14,6 @@ function draw() {
       let noiseG = noise(x * 0.04 * scl, y * 0.06 * scl);
       let noiseB = noise(x * 0.05 * scl, y * 0.03 * scl);
       let c = color(noiseR * 255, noiseG * 255, noiseB * 255);
-      // console.log(noiseVal);
       fill(c);
       noStroke();
       rect(x, y, 1, 1);
