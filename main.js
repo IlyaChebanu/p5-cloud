e2d606cb68175680df50d0254e086fb7102799ad
NotQuiteMelvin <cebanu5665@gmail.com>
2016-12-14 13:52:10 +0000

    Added button for generating new image

--- main.js
+++ main.js
@@ -2,18 +2,18 @@ const scl = 0.05; // How big the clouds are, smaller scale => bigger clouds
 const width = 600; // Width of canvas
 const height = 600; // Height of canvas
 
-// Scales will contain 6 random values from 0 to 0.1 which will be used to scale
-// down randomly the RGB noise values to always create a random pattern
-let scales = [];
-for (let i = 0; i < 6; i++) {
-  // Math.random() returns value from 0 to 1, which if divided by 10 gives a
-  // value between 0 and 0.1
-  scales.push(Math.random() / 10)
-}
 
 function setup() {
   let canvas = createCanvas(width, height);
   background(0);
+  // Scales will contain 6 random values from 0 to 0.1 which will be used to scale
+  // down randomly the RGB noise values to always create a random pattern
+  let scales = [];
+  for (let i = 0; i < 6; i++) {
+    // Math.random() returns value from 0 to 1, which if divided by 10 gives a
+    // value between 0 and 0.1
+    scales.push(Math.random() / 10)
+  }
   for (let x = 0; x < width; x++) {
     for (let y = 0; y < height; y++) {
       // I'm too lazy to think of a better way to do this
